b351dfc254d23f5a234cc247daf66e234f8f5d68
Peter Aryanto <paryanto@integradev.com.au>
2017-10-31 14:34:45 +1100

    First Commit: Generic Data Converter to Read Lines from Text File

---
+++ DataConverter/TextFileLinesToObjectListConverter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DataConverter
+{
+  public class TextFileLinesToObjectListConverter<T>
+  {
+    public delegate void ExceptionHandlingMethod(Exception e);
+
+    private ExceptionHandlingMethod HandleException;
+
+    public TextFileLinesToObjectListConverter(ExceptionHandlingMethod handleException)
+    {
+      this.HandleException = handleException;
+    }
+
+    public List<T> Convert(string textFilePathAndName)
+    {
+      string textFileLine;
+      T objectFromTextFileLine;
+      String[] parameterFromTextFileLine = new String[1];
+      List<T> objectListFromTextFileLines = new List<T>();
+
+      StreamReader textFileStreamReader = null;
+      try
+      {
+        textFileStreamReader = new StreamReader(textFilePathAndName);
+
+        while((textFileLine = textFileStreamReader.ReadLine()) != null)
+        {
+          try
+          {
+            parameterFromTextFileLine[0] = textFileLine;
+            objectFromTextFileLine = (T)Activator.CreateInstance(
+              typeof(T), 
+              parameterFromTextFileLine
+            );
+            objectListFromTextFileLines.Add(objectFromTextFileLine);
+          }
+          catch (Exception e)
+          {
+            if (this.HandleException != null)
+            {
+              this.HandleException(e);
+            }
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        if (this.HandleException != null)
+        {
+          this.HandleException(e);
+        }
+      }
+      finally
+      {
+        if (textFileStreamReader != null)
+        {
+          textFileStreamReader.Close();
+        }
+      }
+
+      return objectListFromTextFileLines;
+    }
+  }
+}
---
+++ text-line-importer-from-text-file/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using DataConverter;
+
+namespace MainProgram
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length != 1)
+      {
+        Console.WriteLine("Please execute: text-line-importer-from-text-file [text-file-path-and-name]");
+        return;
+      }
+
+      TextFileLinesToObjectListConverter<StringWrapper> textFileLinesTostringListConverter =
+        new TextFileLinesToObjectListConverter<StringWrapper>(HandleException);
+      List<StringWrapper> stringListToSort =
+        textFileLinesTostringListConverter.Convert(args[0]);
+
+      if (stringListToSort == null || stringListToSort.Count == 0)
+      {
+        Console.WriteLine("Program is stopped. No data to process.");
+        return;
+      }
+
+      ExportstringsToTextFile(stringListToSort, "text-file-output.txt", true);
+    }
+
+    private class StringWrapper
+    {
+      public string TextData { get; }
+
+      public StringWrapper(string inputText)
+      {
+        this.TextData = inputText;
+      }
+    }
+
+    private static void HandleException(Exception e)
+    {
+      if (e.InnerException != null)
+      {
+        e = e.InnerException;
+      }
+
+      Console.WriteLine(e.GetType().Name + ": " + e.Message);
+    }
+
+    private static void ExportstringsToTextFile(
+      List<StringWrapper> stringsList,
+      string textFilePathAndName,
+      bool displaystringsOnScreen
+    )
+    {
+      StreamWriter textFileWriter = null;
+      try
+      {
+        textFileWriter = new StreamWriter(textFilePathAndName);
+
+        foreach (StringWrapper aString in stringsList)
+        {
+          if (displaystringsOnScreen)
+          {
+            Console.WriteLine(aString.TextData);
+          }
+
+          textFileWriter.WriteLine(aString.TextData);
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("The file could not be written:");
+        Console.WriteLine(e.Message);
+      }
+      finally
+      {
+        if (textFileWriter != null)
+        {
+          textFileWriter.Close();
+        }
+      }
+   }
+  }
+}
